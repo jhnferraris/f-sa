c76590895c19b9e0904592b1ccf2e054a8379b31
jhnferraris <johnmichaelferraris@gmail.com>
2018-09-29 15:58:26 +0800

    Improved initial codebase for Firefly algorithm. TODO: Add SA

--- app.js
+++ app.js
@@ -1,46 +1,90 @@
+const arguments = process.argv;
+if (arguments[2] !== '--problem') {
+  console.log('Missing argument: problem');
+
+  process.exit(1);
+}
+
+const problem = arguments[3];
 const config = require('./config');
 const functions = require('./functions');
-let fireflies = [];
-const fireflyGenerator = () => {
-  return functions[config.generator]();
-};
-
+const fireflies = [];
+const { population } = config.fsaConfig;
+const designFunction = config.functions[problem];
+const { initialAlpha, finalAlpha } = config.globalSearchConfig;
+let alpha = initialAlpha;
 const initializePopulation = () => {
-  while (fireflies.length != config.fireflyPopulation) {
-    fireflies.push(fireflyGenerator());
+  while (fireflies.length != population) {
+    fireflies.push(functions[problem].generator());
   }
 };
 
 const updateLightIntensity = (index, leastFirefly) => {
-  for (let k = 0; k < config.dimension; k++) {
-    fireflies[index] = leastFirefly[k];
+  for (let k = 0; k < designFunction.dimension - 1; k++) {
+    fireflies[index][k] = leastFirefly[k];
   }
-  fireflies[index].fnValue = functions[config.functionName]({ ...fireflies[index] });
+  fireflies[index][designFunction.config.dimension] = functions[designFunction.fn]['function'](...fireflies[index]);
 };
 
-const distance = (leastFirefly, betterFirefly) => {
+const cartesianDistance = (leastFirefly, betterFirefly) => {
   let distance = 0.0;
-  let values;
+  for (let k = 0; k < designFunction.dimension; k++) {
+    distance += Math.pow(leastFirefly[k] - betterFirefly[k], 2);
+  }
+  return Math.sqrt(distance);
 };
-const move = (leastFirefly, betterFirefly) => {
-  const thirdTerm = config.alpha * (Math.random() - 0.5);
-  //(Double.parseDouble("" + ff_I.get(k)) - Double.parseDouble("" + ff_J.get(k))) * (Double.parseDouble("" + ff_I.get(k)) - Double.parseDouble("" + ff_J.get(k)));
-  // const r =
+
+const moveFirefly = (leastFirefly, betterFirefly) => {
+  let newLeastFirefly = new Array(designFunction.dimension);
+  const distance = cartesianDistance(leastFirefly, betterFirefly);
+  const { initialAttractiveness, absorptionCoefficient } = config.globalSearchConfig;
+  for (let k = 0; k < designFunction.dimension; k++) {
+    const secondTerm = initialAttractiveness * Math.exp(-1 * absorptionCoefficient * Math.pow(distance, 2.0)) * (betterFirefly[k] - leastFirefly[k]);
+    const thirdTerm = alpha * (Math.random() - 0.5);
+    newLeastFirefly = leastFirefly[k] + secondTerm + thirdTerm;
+  }
+
+  newLeastFirefly = functions[problem].checkLimits(newLeastFirefly);
+  return newLeastFirefly;
 };
+
 const main = () => {
-  console.log('running algo');
+  console.log('Running F-SA');
   initializePopulation();
   let bestFirefly = fireflies[0];
   let candidateFirefly = null;
-  for (let generationIndex = 0; generationIndex < config.numberOfGenerations; generationIndex++) {
-    for (let i = 0; i < config.fireflyPopulation; i++) {
+  const { globalGeneration } = config.fsaConfig;
+  const output = [];
+  for (let generationIndex = 1; generationIndex <= globalGeneration; generationIndex++) {
+    for (let i = 0; i < population; i++) {
       for (let j = 0; j < i; j++) {
-        if (fireflies[j].fnValue < fireflies[i].fnValue) {
+        const lightIntensityJ = fireflies[j][designFunction.config.dimension];
+        const lightIntensityI = fireflies[i][designFunction.config.dimension];
+        if (lightIntensityJ < lightIntensityI) {
           candidateFirefly = fireflies[j];
+          updateLightIntensity(i, moveFirefly(fireflies[i], fireflies[j]));
+          // TODO Add SA here
+        } else if (lightIntensityI < lightIntensityJ) {
+          candidateFirefly = fireflies[i];
+          updateLightIntensity(j, moveFirefly(fireflies[j], fireflies[i]));
+          // TODO Add SA here
+        } else {
+          updateLightIntensity(j, moveFirefly(fireflies[j], fireflies[i]));
+        }
+
+        if (candidateFirefly[designFunction.dimension] < bestFirefly[designFunction.dimension]) {
+          bestFirefly = candidateFirefly;
         }
       }
     }
+    alpha = finalAlpha + (initialAlpha - finalAlpha) * Math.exp(-1 * generationIndex);
+    output.push({
+      bestFirefly,
+      generationIndex
+    });
   }
+
+  console.log(output);
 };
 
 main();
--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,49 @@
-const { bounds } = require('../config');
+const { functions } = require('../config');
 module.exports = {
-  springFireflyGenerator() {
-    console.log('Running function');
-    let g1, g2, g3, g4, x1, x2, x3, x4;
+  spring: {
+    generator() {
+      console.log('Running Spring function');
+      const { bounds } = functions.spring.config;
+      let g1, g2, g3, g4, x1, x2, x3, x4;
 
-    x1 = bounds[0].lower + Math.random() * (bounds[0].upper - bounds[0].lower);
-    x2 = bounds[1].lower + Math.random() * (bounds[1].upper - bounds[1].lower);
-    x3 = bounds[2].lower + Math.random() * (bounds[2].upper - bounds[2].lower);
-
-    g1 = 1 - Math.pow(x2, 3) * x3 / (7178 * Math.pow(x1, 4));
-    g2 = 4 * Math.pow(x2, 2) - x1 * x2 / (12566 * (x2 * Math.pow(x1, 3) - Math.pow(x1, 4))) + 1 / (5108 * Math.pow(x1, 2)) - 1;
-    g3 = 1 - 140.45 * x1 / (Math.pow(x2, 2) * x3);
-    g4 = (x2 + x1) / 1.5 - 1;
-
-    while (!(g1 <= 0 && g2 <= 0 && g3 <= 0 && g4 <= 0)) {
       x1 = bounds[0].lower + Math.random() * (bounds[0].upper - bounds[0].lower);
       x2 = bounds[1].lower + Math.random() * (bounds[1].upper - bounds[1].lower);
       x3 = bounds[2].lower + Math.random() * (bounds[2].upper - bounds[2].lower);
+
       g1 = 1 - Math.pow(x2, 3) * x3 / (7178 * Math.pow(x1, 4));
       g2 = 4 * Math.pow(x2, 2) - x1 * x2 / (12566 * (x2 * Math.pow(x1, 3) - Math.pow(x1, 4))) + 1 / (5108 * Math.pow(x1, 2)) - 1;
       g3 = 1 - 140.45 * x1 / (Math.pow(x2, 2) * x3);
       g4 = (x2 + x1) / 1.5 - 1;
-    }
 
-    let firefly = {
-      x1,
-      x2,
-      x3,
-      fnValue: spring(x1, x2, x3)
-    };
-    return firefly;
-  },
-  spring(x1, x2, x3) {
-    return (x3 + 2) * x2 * Math.pow(x1, 2);
+      // Limits
+      while (!(g1 <= 0 && g2 <= 0 && g3 <= 0 && g4 <= 0)) {
+        x1 = bounds[0].lower + Math.random() * (bounds[0].upper - bounds[0].lower);
+        x2 = bounds[1].lower + Math.random() * (bounds[1].upper - bounds[1].lower);
+        x3 = bounds[2].lower + Math.random() * (bounds[2].upper - bounds[2].lower);
+        g1 = 1 - Math.pow(x2, 3) * x3 / (7178 * Math.pow(x1, 4));
+        g2 = 4 * Math.pow(x2, 2) - x1 * x2 / (12566 * (x2 * Math.pow(x1, 3) - Math.pow(x1, 4))) + 1 / (5108 * Math.pow(x1, 2)) - 1;
+        g3 = 1 - 140.45 * x1 / (Math.pow(x2, 2) * x3);
+        g4 = (x2 + x1) / 1.5 - 1;
+      }
+
+      let firefly = [x1, x2, x3, this.function(x1, x2, x3)];
+      return firefly;
+    },
+    checkLimits(firefly) {
+      const { bounds, dimension } = functions.spring.config;
+      for (let i = 0; i < dimension - 1; i++) {
+        if (firefly[i] < bounds[i].lower) {
+          firefly[i] = bounds[i].lower;
+        }
+        if (firefly[i] > bounds[i].upper) {
+          firefly[i] = bounds[i].upper;
+        }
+      }
+
+      return firefly;
+    },
+    function(x1, x2, x3) {
+      return (x3 + 2) * x2 * Math.pow(x1, 2);
+    }
   }
 };
